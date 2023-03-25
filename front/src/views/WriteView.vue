15b8b7c4dc55e7c3de5048e57b442d9af463a267
Sangwook Hwang <yoonseol1983@gmail.com>
2023-03-25 23:58:39 +0900

    element plus css ui 적용

--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -5,9 +5,19 @@ const count = ref(0)
 </script>
 
 <template>
-  <p>안녕하세요</p> <button @click="count += 1">버튼을 눌러주세요</button>
-  <p>{{count}}</p>
+  <div>
+    <el-input placeholder="제목을 입력하세요" />
 
+    <div>
+      <div>
+        <el-input type="textarea" rows="15" placeholder="내용을 입력하세요" />
+      </div>
+
+      <div>
+        <el-button type="primary"> 글 작성 완료</el-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
